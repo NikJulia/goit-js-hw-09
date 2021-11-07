fdbfbdeddb7e29e919ff3ae47564e09ec13a6052
NikJulia <74920206+NikJulia@users.noreply.github.com>
2021-11-07 23:43:03 +0200

    01-color-switcher

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,33 @@
+const refs = {
+    startBtn: document.querySelector('button[data-start]'),
+    stopBtn: document.querySelector('button[data-stop]'),
+    body: document.querySelector('body'),
+}
+
+const BODY_COLOR_DELAY = 1000;
+let timeoutId = null;
+refs.startBtn.disabled = false;
+refs.stopBtn.disabled = true;
+
+refs.startBtn.addEventListener('click', onStartBtnClick);
+refs.stopBtn.addEventListener('click', onStopBtnClick);
+
+function onStartBtnClick () {
+    refs.startBtn.disabled = true;
+    refs.stopBtn.disabled = false;
+    timeoutId = setInterval(() => {
+        refs.body.style.backgroundColor = getRandomHexColor();
+      }, BODY_COLOR_DELAY);
+}
+
+function onStopBtnClick () {
+    refs.startBtn.disabled = false;
+    refs.stopBtn.disabled = true;
+    clearInterval(timeoutId);
+}
+
+function getRandomHexColor() {
+    return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+  }
+
 
